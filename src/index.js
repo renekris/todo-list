091930edad1d148064faf152f90d38078abdede2
Renekris <renekrispohlak@gmail.com>
2022-10-06 05:15:49 +0300

    Manage files & add to webpack

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import './index.css';
 import { getTaskList, createTask } from './js/createTask';
-import test from './js/displayController';
+import draw from './js/displayController';
 
 (() => {
     console.log(createTask('test', 'cringe', 'test2', 'cringe2', 'list item 2', 2, 1));
     createTask('test', 'cringe', 'test2', 'cringe2', 'list item 2', 2, 1);
     console.log(getTaskList());
-    test.addTask('test');
+    draw.initialize();
+    draw.addProject('test');
+    draw.addButton();
+    draw.addProfileImage();
 })();
--- src/js/displayController.js
+++ src/js/displayController.js
@@ -1,7 +1,46 @@
+import profilePicture from "../img/temp/profile-picture.png";
+import { createProject } from "./createTask";
+
+// DOM CACHE
+const elContent = document.getElementById('content');
+const elProfilePicture = Array.from(document.getElementsByClassName('profile-picture'))[0];
+
+
+// EVENT LISTENERS
+
+
 const draw = {
-    addTask(task) {
-        console.log(task);
-    }
+    initialize() {
+        createTaskWrapper();
+        this.addProfileImage();
+    },
+    addProject(data) {
+        console.log(data);
+        createProject(data);
+    },
+    addButton() {
+        const elButton = document.createElement('button');
+        elButton.addEventListener('pointerdown', this.addCard);
+
+        const elParentDiv = document.createElement('div');
+        elParentDiv.className = 'add-button';
+        elParentDiv.appendChild(elButton);
+        elContent.appendChild(elParentDiv);
+    },
+    addCard() {
+        console.log('test');
+    },
+    addProfileImage() {
+        elProfilePicture.src = profilePicture;
+    },
+}
+
+
+function createTaskWrapper() {
+    const taskWrapper = document.createElement('div');
+    taskWrapper.className = 'task-wrapper';
+
+    elContent.appendChild(taskWrapper);
 }
 
 export default draw;
