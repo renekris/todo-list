dabdb86f48b7ebcbdd2f0218c9c94358972b4ea7
Renekris <renekrispohlak@gmail.com>
2022-10-06 07:45:54 +0300

    Change displayController structure and add modal

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import './index.css';
 import { getTaskList, createTask } from './js/createTask';
-import draw from './js/displayController';
+import { initialize } from './js/displayController';
 
 (() => {
     console.log(createTask('test', 'cringe', 'test2', 'cringe2', 'list item 2', 2, 1));
     createTask('test', 'cringe', 'test2', 'cringe2', 'list item 2', 2, 1);
     console.log(getTaskList());
-    draw.initialize();
-    draw.addProject('test');
-    draw.addButton();
-    draw.addProfileImage();
+    initialize();
+    // draw.initialize();
+    // draw.addProject('test');
+    // draw.addButton();
+    // draw.addProfileImage();
 })();
--- src/js/displayController.js
+++ src/js/displayController.js
@@ -4,43 +4,59 @@ import { createProject } from "./createTask";
 // DOM CACHE
 const elContent = document.getElementById('content');
 const elProfilePicture = Array.from(document.getElementsByClassName('profile-picture'))[0];
+//
 
 
-// EVENT LISTENERS
 
+function initialize() {
+    addProfileImage();
+    createBase();
+
+}
 
 const draw = {
-    initialize() {
-        createTaskWrapper();
-        this.addProfileImage();
-    },
     addProject(data) {
         console.log(data);
         createProject(data);
     },
-    addButton() {
-        const elButton = document.createElement('button');
-        elButton.addEventListener('pointerdown', this.addCard);
-
-        const elParentDiv = document.createElement('div');
-        elParentDiv.className = 'add-button';
-        elParentDiv.appendChild(elButton);
-        elContent.appendChild(elParentDiv);
-    },
-    addCard() {
-        console.log('test');
-    },
-    addProfileImage() {
-        elProfilePicture.src = profilePicture;
-    },
 }
 
+function createBase() {
+    const elTaskWrapper = elContent.appendChild(createTaskWrapper());
+    const elTaskList = elTaskWrapper.appendChild(createTaskList());
+    const elAddButton = elTaskList.appendChild(createButton());
+
+}
 
 function createTaskWrapper() {
     const taskWrapper = document.createElement('div');
     taskWrapper.className = 'task-wrapper';
-
-    elContent.appendChild(taskWrapper);
+    return taskWrapper;
 }
 
-export default draw;
+function createTaskList() {
+    const taskList = document.createElement('div');
+    taskList.className = 'task-list';
+    return taskList;
+}
+
+function createButton() {
+    const elButton = document.createElement('button');
+    elButton.addEventListener('pointerdown', addCardModal);
+    const elParentDiv = document.createElement('div');
+    elParentDiv.className = 'add-button';
+    elParentDiv.appendChild(elButton);
+    return elParentDiv;
+}
+
+
+
+function addProfileImage() {
+    elProfilePicture.src = profilePicture;
+}
+
+function addCardModal() {
+    console.log('test');
+}
+
+export { initialize };
