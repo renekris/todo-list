c4e225875c574053666eb9324e77aca300536efa
Renekris <renekrispohlak@gmail.com>
2022-10-20 07:17:14 +0300

    Update webpack and prepare for gh-pages

--- webpack.common.js
+++ webpack.common.js
@@ -1,12 +1,7 @@
 const path = require('path');
 
 module.exports = {
-    mode: 'development',
     entry: './src/index.js',
-    devtool: 'source-map',
-    devServer: {
-        static: './dist',
-    },
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
